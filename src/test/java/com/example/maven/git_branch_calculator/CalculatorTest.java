850e3ab79454918fae3783b8d6cecf386c15df73
Aidin Parsiani <aidin.parsiani@gmail.com>
2017-11-29 10:24:59 +0100

    I added testcases for the subtract method

--- src/test/java/com/example/maven/git_branch_calculator/CalculatorTest.java
+++ src/test/java/com/example/maven/git_branch_calculator/CalculatorTest.java
@@ -27,5 +27,20 @@ public class CalculatorTest {
 		assertEquals(c.add(firstNumber, secondNumber), result);
 		}
 	}
-
+	@Test
+	public void testSubtractMethod() {
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+		
+		for (int i = 0; i < 10; i++) {
+		
+		firstNumber = random.nextInt(100);
+		secondNumber = random.nextInt(100);
+		result = firstNumber - secondNumber;
+		LOG.info("Testing the method subtract with " + firstNumber + " and " + secondNumber);
+		assertEquals(c.subtract(firstNumber, secondNumber), result);
+		}
+	}
 }
