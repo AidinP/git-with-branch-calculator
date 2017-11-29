50c7a7ecb5f5ae2e93026ff7fae5f6d3477bfd91
Aidin Parsiani <aidin.parsiani@gmail.com>
2017-11-29 09:58:53 +0100

    Second Commit-forgot to do add

---
+++ src/main/java/com/example/maven/git_branch_calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.maven.git_branch_calculator;
+
+public class Calculator {
+	public int add(int firstNumber, int secondNumber) {
+		return firstNumber + secondNumber;
+		
+		
+		
+	}
+
+}
